ec7cd02ab22c26632884ae283fe503adc6f83d70
Masri-Programmer <masri_mohamad@protonmail.com>
2025-09-11 16:21:00 +0200

    chore: Remove hardcoded base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/masri-web/',
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
